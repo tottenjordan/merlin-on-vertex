652ab10823a18deaddfe90a9a1180c5f53e7e72c
Jordan Totten <39886184+tottenjordan@users.noreply.github.com>
2022-10-27 13:38:42 -0500

    2 tower

---
+++ src/trainer/two_tower_model.py
@@ -0,0 +1,56 @@
+from typing import List, Any
+
+from merlin.models.utils.example_utils import workflow_fit_transform
+from merlin.schema.tags import Tags
+import merlin.models.tf as mm
+from merlin.models.tf.outputs.base import DotProduct, MetricsFn, ModelOutput
+
+import nvtabular as nvt
+import nvtabular.ops as ops
+
+import logging
+
+import tensorflow as tf
+
+
+def create_two_tower(
+    train_dir: str,
+    valid_dir: str,
+    workflow_dir: str,
+    layer_sizes: List[Any] = [512, 256, 128],
+):
+    
+    #=========================================
+    # get workflow details
+    #=========================================
+    workflow = nvt.Workflow.load(workflow_dir) # gs://spotify-merlin-v1/nvt-preprocessing-spotify-v24/nvt-analyzed
+    
+    schema = workflow.output_schema
+    embeddings = ops.get_embedding_sizes(workflow)
+    
+    user_schema = schema.select_by_tag(Tags.USER)
+    user_inputs = mm.InputBlockV2(user_schema)
+    
+    #=========================================
+    # build towers
+    #=========================================
+    query = mm.Encoder(user_inputs, mm.MLPBlock(layer_sizes))
+    
+    item_schema = schema.select_by_tag(Tags.ITEM)
+    item_inputs = mm.InputBlockV2(
+        item_schema,
+    )
+    candidate = mm.Encoder(item_inputs, mm.MLPBlock(layer_sizes))
+    
+    model = mm.RetrievalModelV2(
+        query=query,
+        candidate=candidate,
+        output=mm.ContrastiveOutput(
+            to_call=DotProduct(),
+            negative_samplers="in-batch",
+            schema=item_schema.select_by_tag(Tags.ITEM_ID),
+            candidate_name="item",
+        )
+    )
+    
+    return model
